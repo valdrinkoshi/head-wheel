75bed1143c3eb79adc9cd06091364f6f019c609e
valdrinkoshi <valdrin@google.com>
2017-06-23 18:38:55 -0700

    game boundaries, smaller vendor

--- _vendor.js
+++ _vendor.js
@@ -4,9 +4,9 @@ require('aframe');
 var physics = require('aframe-physics-system');
 physics.registerAll();
 
-var extras = require('aframe-extras');
-// Register a particular package, and its dependencies.
-extras.controls.registerAll();
+// var extras = require('aframe-extras');
+// // Register a particular package, and its dependencies.
+// extras.controls.registerAll();
 
-// Register everything.
-extras.registerAll();
+// // Register everything.
+// extras.registerAll();
