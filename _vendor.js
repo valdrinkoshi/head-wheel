250475a101b520e04cad3e68b846d6f9094f5c82
valdrinkoshi <valdrin@google.com>
2017-06-26 11:41:41 -0700

    one kick only, restart after 7s

--- _vendor.js
+++ _vendor.js
@@ -1,12 +1,12 @@
 require('webrtc-adapter');
 require('aframe');
 
-var physics = require('aframe-physics-system');
-physics.registerAll();
+// var physics = require('aframe-physics-system');
+// physics.registerAll();
 
-// var extras = require('aframe-extras');
-// // Register a particular package, and its dependencies.
-// extras.controls.registerAll();
+var extras = require('aframe-extras');
+// Register a particular package, and its dependencies.
+extras.controls.registerAll();
 
-// // Register everything.
-// extras.registerAll();
+// Register everything.
+extras.registerAll();
