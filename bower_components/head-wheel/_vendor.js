adbf5c85fc53c49c12d5a0d674cdec7feced3849
valdrinkoshi <valdrin@google.com>
2017-06-30 17:26:27 -0700

    seed gh-pages

---
+++ bower_components/head-wheel/_vendor.js
@@ -0,0 +1,12 @@
+require('webrtc-adapter');
+require('aframe');
+
+// var physics = require('aframe-physics-system');
+// physics.registerAll();
+
+var extras = require('aframe-extras');
+// Register a particular package, and its dependencies.
+extras.controls.registerAll();
+
+// Register everything.
+extras.registerAll();
